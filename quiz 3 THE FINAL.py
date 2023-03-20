a6aad07448b955310d5b5e9cb16c3ecaf757465e
urmumeatsglue <gurshaan_ban@student.asdah.school.nz>
2023-03-21 12:32:54 +1300

    i have added coments

---
+++ quiz 3 THE FINAL.py
@@ -0,0 +1,148 @@
+
+#Created by Gurshaan Singh bansal
+#Date 27/02/2023
+#Demonstrate asking the user a question, provide multiple choice awnsers, get the user's awnser, check if its correct
+
+
+#asking the user for their name to start this quiz
+name = input("What is your name? \n")
+print("\n Hi {} Are you ready to try the most fun car quiz in the world? \n".format(name))
+points = 0 
+
+def check(options , user_input):
+    if user_input in options:
+       return True
+    else:
+       return False 
+#asking user to confirm if the user wants to play he quiz or not    
+options = ["yes" , "a" ]
+wants_to_play = input ("\n A) Yes \n B) No\n \n").lower()
+
+if check(options, wants_to_play):
+    print("\n Okay mate lets start! \n")
+    points += 1
+elif wants_to_play == "no" or wants_to_play == "b":
+    print("\n Too bad you are doing it anyways\n")
+    points -= 1
+#question one
+options = ["d" , "bugatti chiron super sport 300+" , "bugatti" , "chiron super sport 300+" , "chiron super sport 300" , "chiron" , "super sport" , "super" , "sport" , "300+" , "300" , "Bugatti Chiron Super Sport" , "bugatti chiron"]
+question = input("What is the fastest car in the world?  \n A) Hennessey Venom F5 \n B) Nissan Skyline r34 GTR \n C) Toyota Supra mk4 (a90) \n D) Bugatti Chiron Super Sport 300+ \n \n").lower().strip()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n Wrong awnser! \n")
+
+#question two
+options = ["a" ,"mazda" , "mazda miata mx5" ,"miata" , "mx5" ]
+question = input ("What car is from Japan?  \n A) Mazda Miata MX5 \n B) Chevrolet Corvette \n C) BMW M4 \n D) Mercades AMG GTR \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question three
+options = ["c" , "volvo" , ]
+question = input ("Which manufacturer made seatbelts? \n A) Toyota \n B) Ford \n C) Volvo \n D) Hyundai \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question four
+options = ["b", "toyota" , ]
+question = input ("What car company did Kiichiro Toyoda create? \n A) Opel \n B) Toyota \n C) Nissan \n D) Peugeot \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question five    
+options = ["d" ,"volkswagen" , "vw" ,]
+question = input ("What car company owns Bugatti? \n A) Lada \n B) Lotus \n C) Mclaren \n D) Volkswagen \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question six
+options = ["b" , "tractors"]
+question = input ("What did Lamborghini make before cars? \n A) Trucks \n B) Tractors \n C) Motorcycles\n D) Airplanes \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question seven
+options = ["a" , "currywurst"]
+question = input ("What food item does VW make? \n A) Currywurst \n B) Burgers \n C) Yorkshire pudding \n D) Pies \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\nYou got the wrong awnser \n")
+
+#question eight
+options = ["c" , "tesla"]
+question = input ("What car company does Elon Musk own? \n A) Lucid air \n B) Rivian \n C) Tesla \n D) BYD \n \n").lower()
+
+if check(options, question):
+    print("\n You got the right awnser! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question nine
+options = ["a" , "bull"]
+question = input ("Which animal features in the logo for Lamborghini? \n A) Bull \n B) Horse \n C) Cow \n D) Frog \n \n").lower()
+
+if check(options, question):
+    print("\n hooray it is correct!\n ")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#question ten
+options = ["d" , "rolls royce " , "rr" , "rolls" , "royce"]
+question = input ("Which iconic car manufacturer also made airplane engines? \n A) Acura \n B) Honda \n C) Suzuki \n D) Rolls Royce \n \n").lower()
+
+if check(options, question):
+    print("\n Keep going! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+#question eleven
+options = ["c" , "toyota corolla" , "corolla " , "toyota"]
+question = input ("What is the world's all-time bestselling car?  \n A) Honda Civic \n B) Chevrolet Camero \n C) Toyota Corolla \n D) Dodge Dart \n \n").lower()
+
+if check(options, question):
+    print("\n Good job! \n")
+    points += 1
+else:
+    print("\n You got the wrong awnser \n")
+
+#telling user how many they got right
+print("\n Congrats you got {} out of 11 right good job. \n".format(points))
+
+#remove one pint if user did not wanted to play
+if wants_to_play == "no":
+    print ("I took away one point because you said no to playing the quiz. IT TOOK ME AGES.")
+
+#thanking user for playing the quiz
+print("\n Bye {} thank you for participating in my little fun quiz, I will see you in my next quiz, bye \n".format(name))
+points = 0 
+
+#author credentials
+print ("\n Created by Gurshaan Singh Bansal \n published in 2023 \n")
--- quiz.py
+++ quiz.py
@@ -5,28 +5,28 @@
 
 
 #asking the user for their name to start this quiz
-name = input("What is your name?")
-print("Hi {} Are you ready to try the least fun puzzle quiz?".format(name))
+name = input("What is your name? \n")
+print("Hi {} Are you ready to try the least fun puzzle quiz?\n".format(name))
 points = 0 
 
 def check(options , user_input):
     if user_input in options:
        return True
     else:
-       return False
+       return False 
 
-options = ["d" , "D" , "Bugatti Chiron Super Sport 300+ " , "Bugatti" , "bugatti"]
-Question = input("What is the fastest car in the world?  \n A) Hennessey Venom F5 \n B) Nissan Skyline r34 GTR \n C) Toyota Supra mk4 (a90) \n D) Bugatti Chiron Super Sport 300+ ")
+options = ["d" , "bugatti chiron super sport 300+ " , "bugatti" , "chiron super sport 300+" , "chiron super sport 300" , "chiron" , ]
+Question = input("What is the fastest car in the world?  \n A) Hennessey Venom F5 \n B) Nissan Skyline r34 GTR \n C) Toyota Supra mk4 (a90) \n D) Bugatti Chiron Super Sport 300+ \n \n").lower()
 
 if check(options, Question):
-    print("You got the right awnser!")
+    print("\n You got the right awnser! \n")
     points += 1
 else:
     print("Wrong awnser!")
 
 
-options = ["a", "A" , "Mazda" , "mazda" , "Mazda Miata MX5" , "mazda miata mx5" ,"miata" , "mx5" ]
-Question = input ("What car is from Japan?  \n A) Mazda Miata MX5 \n B) Chevrolet Corvette \n C) BMW M4 \n D) Mercades AMG GTR ") 
+options = ["a" ,"mazda" , "mazda miata mx5" ,"miata" , "mx5" ]
+Question = input ("What car is from Japan?  \n A) Mazda Miata MX5 \n B) Chevrolet Corvette \n C) BMW M4 \n D) Mercades AMG GTR \n ").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -35,8 +35,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["c", "C" , "volvo" , "Volvo" , ]
-Question = input ("Which manufacturer made seatbelts? \n A) Toyota \n B) Ford \n C) Volvo \n D) Hyundai ") 
+options = ["c" , "volvo" , ]
+Question = input ("Which manufacturer made seatbelts? \n A) Toyota \n B) Ford \n C) Volvo \n D) Hyundai \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -45,8 +45,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["b", "B" , "Toyota" , "toyota" , ]
-Question = input ("What car company did Kiichiro Toyoda create? \n A) Opel \n B) Toyota \n C) Nissan \n D) Peugeot ") 
+options = ["b", "toyota" , ]
+Question = input ("What car company did Kiichiro Toyoda create? \n A) Opel \n B) Toyota \n C) Nissan \n D) Peugeot \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -55,8 +55,8 @@ else:
     print("You got the wrong awnser")
 
     
-options = ["d", "D" , "Volkswagen" , "volkswagen" , "vw" , "VW" , "Vw" , "vW"]
-Question = input ("What car company owns Bugatti? \n A) Lada \n B) Lotus \n C) Mclaren \n D) Volkswagen ") 
+options = ["d" ,"volkswagen" , "vw" ,]
+Question = input ("What car company owns Bugatti? \n A) Lada \n B) Lotus \n C) Mclaren \n D) Volkswagen \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -65,8 +65,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["B", "b" , "Tractors" , "tractors"]
-Question = input ("What did Lamborghini make before cars? \n A) Trucks \n B) Tractors \n C) Motorcycles\n D) Airplanes ") 
+options = ["b" , "tractors"]
+Question = input ("What did Lamborghini make before cars? \n A) Trucks \n B) Tractors \n C) Motorcycles\n D) Airplanes \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -75,8 +75,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["A", "a" , "Currywurst" , "currywurst"]
-Question = input ("What food item does VW make? \n A) Currywurst \n B) Burgers \n C) Yorkshire pudding \n D) Pies  ") 
+options = ["a" , "currywurst"]
+Question = input ("What food item does VW make? \n A) Currywurst \n B) Burgers \n C) Yorkshire pudding \n D) Pies \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -85,8 +85,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["C", "c" , "tesla" , "Tesla"]
-Question = input ("What car company does Elon Musk own? \n A) Lucid air \n B) Rivian \n C) Tesla \n D) BYD ") 
+options = ["c" , "tesla"]
+Question = input ("What car company does Elon Musk own? \n A) Lucid air \n B) Rivian \n C) Tesla \n D) BYD \n").lower()
 
 if check(options, Question):
     print("You got the right awnser!")
@@ -95,8 +95,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["a", "A" , "bull" , "Bull"]
-Question = input ("Which animal features in the logo for Lamborghini? \n A) Bull \n B) Horse \n C) Cow \n D) Frog ") 
+options = ["a" , "bull"]
+Question = input ("Which animal features in the logo for Lamborghini? \n A) Bull \n B) Horse \n C) Cow \n D) Frog \n").lower()
 
 if check(options, Question):
     print("hooray it is correct!")
@@ -105,8 +105,8 @@ else:
     print("You got the wrong awnser")
 
 
-options = ["d", "D" , "Rolls Royce " , "Rolls royce " , "rolls royce " , "rr" , "RR"]
-Question = input ("Which iconic car manufacturer also made airplane engines? \n A) Acura \n B) Honda \n C) Suzuki \n D) Rolls Royce  ") 
+options = ["d" , "rolls royce " , "rr" ,]
+Question = input ("Which iconic car manufacturer also made airplane engines? \n A) Acura \n B) Honda \n C) Suzuki \n D) Rolls Royce \n").lower()
 
 if check(options, Question):
     print("Keep going!")
@@ -115,7 +115,7 @@ else:
     print("You got the wrong awnser")
 
 options = ["c", "C" , "toyota corolla" , "Toyota Corolla" , "corolla " , "Corolla" , "Toyota", "toyota"]
-Question = input ("What is the world's all-time bestselling car?  \n A) Honda Civic \n B) Chevrolet Camero \n C) Toyota Corolla \n D) Dodge Dart ") 
+Question = input ("What is the world's all-time bestselling car?  \n A) Honda Civic \n B) Chevrolet Camero \n C) Toyota Corolla \n D) Dodge Dart \n").lower()
 
 if check(options, Question):
     print("Good job!")
